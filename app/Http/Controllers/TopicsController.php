bd85da629cc25859ae07dd60cbdf8d83e1e5031b
gaowanyao <1528667112@qq.com>
2018-04-18 15:17:54 +0800

    Topic order

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,12 +14,14 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request,Topic $topic)
 	{
 //        为了读取 user 和 category，每次的循环都要查一下 users 和 categories 表，在本例子中我们查询了 30 条话题数据，那么最终我需要执行的查询语句就是 30 * 2 + 1 = 61 条语句。如果我第一次查询出来的是 N 条记录，那么最终需要执行的 SQL 语句就是 N+1 次：
 //        https://d.laravel-china.org/docs/5.4/eloquent-relationships#eager-loading
 //        方法 with() 提前加载了我们后面需要用到的关联属性 user 和 category，并做了缓存。后面即使是在遍历数据时使用到这两个关联属性，数据已经被预加载并缓存，因此不会再产生多余的 SQL 查询：
-        $topics = Topic::with('user','category')->paginate(10);
+
+//        dump($request->order);
+        $topics = $topic->withOrder($request->order)->paginate(10);
 		return view('topics.index', compact('topics'));
 	}
 
