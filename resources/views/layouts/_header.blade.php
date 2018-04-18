8f43f1a3cc0d54b53a83653d1ccf1cc13ad2b2db
gaowanyao <1528667112@qq.com>
2018-04-18 14:37:07 +0800

    Category topic list 话题分类

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -18,8 +18,21 @@
 
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
+            {{--如果传参满足指定条件 ($condition) ，此函数将返回 $activeClass，否则返回 $inactiveClass。--}}
+            {{--此扩展包提供了一批函数让我们更方便的进行 $condition 判断：--}}
+            {{--if_route() - 判断当前对应的路由是否是指定的路由；--}}
+            {{--if_route_param() - 判断当前的 url 有无指定的路由参数。--}}
+            {{--if_query() - 判断指定的 GET 变量是否符合设置的值；--}}
+            {{--if_uri() - 判断当前的 url 是否满足指定的 url；--}}
+            {{--if_route_pattern() - 判断当前的路由是否包含指定的字符；--}}
+            {{--if_uri_pattern() - 判断当前的 url 是否含有指定的字符；--}}
+            
             <ul class="nav navbar-nav">
-
+                <li class="{{ active_class(if_route('topics.index')) }}"><a href="{{ route('topics.index') }}">话题</a></li>
+                <li class="{{ active_class(if_route('categories.show') && if_route_param('category',1)) }}"><a href="{{ route('categories.show',1) }}">分享</a></li>
+                <li class="{{ active_class(if_route('categories.show') && if_route_param('category',2)) }}"><a href="{{ route('categories.show',2) }}">教程</a></li>
+                <li class="{{ active_class(if_route('categories.show') && if_route_param('category',3)) }}"><a href="{{ route('categories.show',3) }}">问答</a></li>
+                <li class="{{ active_class(if_route('categories.show') && if_route_param('category',4)) }}"><a href="{{ route('categories.show',4) }}">公告</a></li>
             </ul>
 
             <!-- Right Side Of Navbar -->
