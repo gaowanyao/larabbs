5600b4e101b5821cf931b533c7f2f8aa06d6cea0
gaowanyao <1528667112@qq.com>
2018-04-18 10:54:31 +0800

    Generated topics 代码生成器生成话题

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,4 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 //Route::resource('users',"UsersController",['only'=>['show','update','edit']]);
 Route::resource('users',"UsersController");
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
