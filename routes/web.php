9f347c485b6e6e448ce807be85d5092051d87be5
gaowanyao <1528667112@qq.com>
2018-04-20 22:44:35 +0800

    Auto translate slug

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,10 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 //Route::resource('users',"UsersController",['only'=>['show','update','edit']]);
 Route::resource('users',"UsersController");
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
+
 Route::resource('categories', 'CategoriesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
