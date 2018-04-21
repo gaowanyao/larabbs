9d13fe96466fefccd3f9e6071c58ef64ce0b7f71
gaowanyao <1528667112@qq.com>
2018-04-21 23:43:12 +0800

    Notify author when topic replied

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,26 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Models\Topic;
+use Auth;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+//    use Notifiable;
+    use Notifiable{
+        notify as protected laravelNotify;
+    }
+//我们对 notify() 方法做了一个巧妙的重写，
+//现在每当你调用 $user->notify() 时，
+// users 表里的 notification_count 将自动 +1。
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -40,6 +56,14 @@ class User extends Authenticatable
 
     public function replies(){
         return $this->hasMany(Reply::class);
+    }
+
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
     }
 
 }
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,7 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
-
+use App\Notifications\TopicReplied;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -17,7 +17,13 @@ class ReplyObserver
 
     public function created(Reply $reply)
     {
-        $reply->topic->increment('reply_count',1);
+        $topic = $reply->topic;
+        $topic->increment('reply_count',1);
+        // 通知作者话题被回复了
+        $topic->user->notify(new TopicReplied($reply));
+
+//        默认的 User 模型中使用了 trait —— Notifiable，它包含着一个可以用来发通知的方法 notify() ，此方法接收一个通知实例做参数。虽然 notify() 已经很方便，但是我们还需要对其进行定制，我们希望每一次在调用 $user->notify() 时，自动将 users 表里的 notification_count +1 ，这样我们就能跟踪用户未读通知了。
+
     }
 
     public function updating(Reply $reply)
